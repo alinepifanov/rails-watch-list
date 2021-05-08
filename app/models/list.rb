580e6e8d10988cd34acf1dad17c78a93161c7ac6
alin epifanov <alin.epifanov@yahoo.com>
2021-05-08 12:38:47 +0200

    Watch List

---
+++ app/models/list.rb
@@ -0,0 +1,9 @@
+class List < ApplicationRecord
+    validates :name, presence: :true, uniqueness: :true
+	has_many :bookmarks
+	has_many :movies, {:through=>:bookmarks, :dependent=>:destroy, :source=>"movie"}
+	
+end
+
+
+# {:through=>:bookmarks, :dependent=>:destroy, :source=>"movie"}
